6b84f771bcc3b6aaeea83fd3068064b193845fe1
Farmaan-Malik <farmaanmalik285@gmail.com>
2024-10-30 14:59:10 +0530

    resolveeeee

--- screens/HomeScreen/HomeScreen.js
+++ screens/HomeScreen/HomeScreen.js
@@ -222,6 +222,7 @@ const HomeScreen = () => {
                 comments={item.comments}
                 bookmarks={item.bookmarks}
               />
+              
             );
           }}
         />
